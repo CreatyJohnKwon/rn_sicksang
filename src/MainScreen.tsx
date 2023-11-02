50f08e75aebfa96297d7c13844c49bd8ff980058
CreatyJohn <vmfodzl1125@naver.com>
2023-11-02 18:27:08 +0900

    push all 2023-11-02

---
+++ src/MainScreen.tsx
@@ -0,0 +1,63 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Text, StatusBar } from 'react-native';
+//npm install --save-dev @types/react-native-vector-icons
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+    return <Text>홈화면</Text>;
+}
+
+function ProfileScreen() {
+    return <Text>프로필 화면</Text>;
+}
+
+function MainScreen() {
+
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle="dark-content" backgroundColor="white"/>
+            <Tab.Navigator
+                initialRouteName="Home"
+                screenOptions={{
+                    headerShown: false,
+                    tabBarActiveTintColor: 'black',
+                    tabBarInactiveTintColor: 'lightgray',
+                    tabBarLabelPosition: 'below-icon',
+                }}>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        title: '홈',
+                        tabBarIcon: ({ color, size }) => (
+                            <Icon name={'home'} color={color} size={size} />
+                        ),
+                        tabBarLabelStyle: {
+                            fontSize: 15,
+                            marginBottom: 5,
+                        },
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                        title: '프로필',
+                        tabBarIcon: ({ color, size }) => (
+                            <Icon name={'account'} color={color} size={size} />
+                        ),
+                        tabBarLabelStyle: {
+                            fontSize: 15,
+                            marginBottom: 5,
+                        },
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MainScreen;
